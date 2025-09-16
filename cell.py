5d334d5dae5153e3254dc2638df6ddeb6f8378d1
Johannes Gustafsson <johannes.gustafsson@elev.ga.ntig.se>
2025-09-16 08:53:33 +0200

    Add draw function to cell class

--- cell.py
+++ cell.py
@@ -18,3 +18,23 @@ class Cell:
 
         # Draw the cell
         self.rect = pygame.Rect(xCell * cellSize, yCell * cellSize, cellSize, cellSize)
+
+    # Draw the cell 
+    def draw(self, surface):
+        if self.revealed:
+            # If the cell is revealed, fill it with gray color
+            pygame.draw.rect(surface, colors["GRAY"], self.rect)
+            if self.hasMine:
+                # If the cell has a mine, fill it with a red circle
+                pygame.draw.circle(surface, colors["RED"], self.rect.center, cellSize // 4)
+            elif self.adjacentMines > 0:
+                # If there are other mines next to it, display the number in black text on the cell.
+                font = pygame.font.SysFont(None, 24)
+                text = font.render(str(self.adjacentMines), True, colors["BLACK"])
+                surface.blit(text, text.get_rect(center=self.rect.center))
+        else:
+            # If the cell is not revealed, fill it with white color
+            pygame.draw.rect(surface, colors["WHITE"], self.rect)
+            # If the cell is flagged, draw a blue circle
+            if self.flagged:
+                pygame.draw.circle(surface, colors["BLUE"], self.rect.center, cellSize // 4)
